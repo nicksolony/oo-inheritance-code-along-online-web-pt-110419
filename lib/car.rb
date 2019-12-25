e0b07ebc9ee96177d2d49e114892cc6a6a8d0e9c
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 20:26:34 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,11 @@
 require_relative "./vehicle.rb"
 
 class Car < Vehicle
+<<<<<<< HEAD
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
+=======
+>>>>>>> 088a325424b7ca0b71dceb0473bc05e20b56add3
   
 end
